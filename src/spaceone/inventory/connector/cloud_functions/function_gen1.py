17dd99a6d32da7409184ef67cc6a02f0e660f5b1
seolmin <seolmin@mz.co.kr>
2022-11-27 20:13:59 +0900

    feat: create connector in cloud function of gen1 (#23)
    Signed-off-by: Seolmin Kwon <seolmin@mz.co.kr>

---
+++ src/spaceone/inventory/connector/cloud_functions/function_gen1.py
@@ -0,0 +1,29 @@
+import logging
+
+from spaceone.inventory.libs.connector import GoogleCloudConnector
+
+__all__ = ['FunctionGen1Connector']
+_LOGGER = logging.getLogger(__name__)
+
+
+class FunctionGen1Connector(GoogleCloudConnector):
+    google_client_service = 'cloudfunctions'
+    version = 'v1'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def list_functions(self):
+        functions = []
+        query = {'parent': self._make_parent()}
+        functions_service = self.client.projects().locations().functions()
+        request = functions_service.list(**query)
+
+        while request is not None:
+            response = request.execute()
+            functions = response.get('functions', [])
+            request = functions_service.list_next(previous_request=request, previous_response=response)
+        return functions
+
+    def _make_parent(self):
+        return f'projects/{self.project_id}/locations/-'
