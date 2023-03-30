7763a06367adf61c250150ec6a9f9de3ab76c28d
seolmin <seolmin@megazone.com>
2023-03-30 17:36:16 +0900

    fix: refactor recommender manager and model because of usability
    Signed-off-by: seolmin <seolmin@megazone.com>

---
+++ src/spaceone/inventory/model/recommender/recommendation/redefined_data.py
@@ -0,0 +1,42 @@
+from schematics import Model
+from schematics.types import ModelType, ListType, StringType, IntType, FloatType
+from spaceone.inventory.libs.schema.cloud_service import BaseResource
+
+
+class RedefinedInsight(Model):
+    description = StringType()
+    severity = StringType()
+    category = StringType(choices=(
+        'CATEGORY_UNSPECIFIED', 'COST', 'SECURITY', 'PERFORMANCE', 'MANAGEABILITY', 'SUSTAINABILITY', 'RELIABILITY'
+    ))
+
+
+class RedefinedRecommendation(Model):
+    description = StringType()
+    state = StringType(choices=('STATE_UNSPECIFIED', 'ACTIVE', 'CLAIMED', 'SUCCEEDED', 'FAILED', 'DISMISSED'))
+    affected_resource = StringType()
+    location = StringType()
+    priority_level = StringType()
+    operations = StringType()
+    cost = FloatType()
+    cost_savings = StringType()
+    insights = ListType(ModelType(RedefinedInsight), default=[])
+
+
+class Recommender(BaseResource):
+    name = StringType()
+    id = StringType()
+    description = StringType()
+    state = StringType(choices=('OK', 'Warning', 'Error'))
+    category = StringType(choices=(
+        'CATEGORY_UNSPECIFIED', 'COST', 'SECURITY', 'PERFORMANCE', 'MANAGEABILITY', 'SUSTAINABILITY', 'RELIABILITY'
+    ))
+    resource_count = IntType()
+    cost_savings = StringType()
+    recommendations = ListType(ModelType(RedefinedRecommendation), default=[])
+
+    def reference(self):
+        return {
+            "resource_id": self.id,
+            "external_link": f'https://console.cloud.google.com/home/recommendations'
+        }
