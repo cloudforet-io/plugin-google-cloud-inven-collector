69d084b12df86ce66cb5e76a3d23c0dab9edbe81
seolmin <seolmin@megazone.com>
2024-04-08 16:16:14 +0900

    refactor: apply black formatter in BigQuery service

--- src/spaceone/inventory/connector/bigquery/sql_workspace.py
+++ src/spaceone/inventory/connector/bigquery/sql_workspace.py
@@ -2,32 +2,33 @@ import logging
 
 from spaceone.inventory.libs.connector import GoogleCloudConnector
 
-__all__ = ['SQLWorkspaceConnector']
+__all__ = ["SQLWorkspaceConnector"]
 _LOGGER = logging.getLogger(__name__)
 
 
 class SQLWorkspaceConnector(GoogleCloudConnector):
-    google_client_service = 'bigquery'
-    version = 'v2'
+    google_client_service = "bigquery"
+    version = "v2"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def list_dataset(self, **query):
         dataset_list = []
-        query.update({'projectId': self.project_id, 'all': True})
+        query.update({"projectId": self.project_id, "all": True})
         request = self.client.datasets().list(**query)
         while request is not None:
             response = request.execute()
-            for dataset in response.get('datasets', []):
+            for dataset in response.get("datasets", []):
                 dataset_list.append(dataset)
-            request = self.client.datasets().list_next(previous_request=request, previous_response=response)
+            request = self.client.datasets().list_next(
+                previous_request=request, previous_response=response
+            )
 
         return dataset_list
 
     def get_dataset(self, dataset_id, **query):
-        query.update({'projectId': self.project_id,
-                      'datasetId': dataset_id})
+        query.update({"projectId": self.project_id, "datasetId": dataset_id})
         response = {}
         response = self.client.datasets().get(**query).execute()
 
@@ -35,15 +36,17 @@ class SQLWorkspaceConnector(GoogleCloudConnector):
 
     def list_job(self, **query):
         job_list = []
-        query.update({'projectId': self.project_id,
-                      'allUsers': True,
-                      'projection': 'full'})
+        query.update(
+            {"projectId": self.project_id, "allUsers": True, "projection": "full"}
+        )
         request = self.client.jobs().list(**query)
         while request is not None:
             response = request.execute()
-            for job in response.get('jobs', []):
+            for job in response.get("jobs", []):
                 job_list.append(job)
-            request = self.client.jobs().list_next(previous_request=request, previous_response=response)
+            request = self.client.jobs().list_next(
+                previous_request=request, previous_response=response
+            )
 
         return job_list
 
@@ -53,31 +56,34 @@ class SQLWorkspaceConnector(GoogleCloudConnector):
 
         while request is not None:
             response = request.execute()
-            for project in response.get('projects', []):
+            for project in response.get("projects", []):
                 project_list.append(project)
-            request = self.client.projects().list_next(previous_request=request, previous_response=response)
+            request = self.client.projects().list_next(
+                previous_request=request, previous_response=response
+            )
 
         return project_list
 
     def list_tables(self, dataset_id, **query):
         table_list = []
 
-        query.update({'projectId': self.project_id,
-                      'datasetId': dataset_id})
+        query.update({"projectId": self.project_id, "datasetId": dataset_id})
 
         request = self.client.tables().list(**query)
         while request is not None:
             response = request.execute()
-            for table in response.get('tables', []):
+            for table in response.get("tables", []):
                 table_list.append(table)
-            request = self.client.tables().list_next(previous_request=request, previous_response=response)
+            request = self.client.tables().list_next(
+                previous_request=request, previous_response=response
+            )
 
         return table_list
 
     def get_tables(self, dataset_id, table_id, **query):
-        query.update({'projectId': self.project_id,
-                      'datasetId': dataset_id,
-                      'tableId': table_id})
+        query.update(
+            {"projectId": self.project_id, "datasetId": dataset_id, "tableId": table_id}
+        )
         response = {}
         response = self.client.tables().get(**query).execute()
 
--- src/spaceone/inventory/manager/bigquery/sql_workspace_manager.py
+++ src/spaceone/inventory/manager/bigquery/sql_workspace_manager.py
@@ -4,20 +4,26 @@ import time
 from spaceone.inventory.libs.manager import GoogleCloudManager
 from spaceone.inventory.libs.schema.base import ReferenceModel
 from spaceone.inventory.connector.bigquery.sql_workspace import SQLWorkspaceConnector
-from spaceone.inventory.model.bigquery.sql_workspace.cloud_service import BigQueryWorkSpace, SQLWorkSpaceResource, \
-    SQLWorkSpaceResponse, ProjectModel
-from spaceone.inventory.model.bigquery.sql_workspace.cloud_service_type import CLOUD_SERVICE_TYPES
+from spaceone.inventory.model.bigquery.sql_workspace.cloud_service import (
+    BigQueryWorkSpace,
+    SQLWorkSpaceResource,
+    SQLWorkSpaceResponse,
+    ProjectModel,
+)
+from spaceone.inventory.model.bigquery.sql_workspace.cloud_service_type import (
+    CLOUD_SERVICE_TYPES,
+)
 from datetime import datetime
 
 _LOGGER = logging.getLogger(__name__)
 
 
 class SQLWorkspaceManager(GoogleCloudManager):
-    connector_name = 'SQLWorkspaceConnector'
+    connector_name = "SQLWorkspaceConnector"
     cloud_service_types = CLOUD_SERVICE_TYPES
 
     def collect_cloud_service(self, params):
-        _LOGGER.debug(f'** Big Query SQL Workspace START **')
+        _LOGGER.debug(f"** Big Query SQL Workspace START **")
         start_time = time.time()
         """
         Args:
@@ -34,14 +40,16 @@ class SQLWorkspaceManager(GoogleCloudManager):
         collected_cloud_services = []
         error_responses = []
 
-        secret_data = params['secret_data']
-        project_id = secret_data['project_id']
+        secret_data = params["secret_data"]
+        project_id = secret_data["project_id"]
 
         ##################################
         # 0. Gather All Related Resources
         # List all information through connector
         ##################################
-        big_query_conn: SQLWorkspaceConnector = self.locator.get_connector(self.connector_name, **params)
+        big_query_conn: SQLWorkspaceConnector = self.locator.get_connector(
+            self.connector_name, **params
+        )
         data_sets = big_query_conn.list_dataset()
         projects = big_query_conn.list_projects()
 
@@ -54,59 +62,75 @@ class SQLWorkspaceManager(GoogleCloudManager):
                 update_bq_dt_tables = []
                 table_schemas = []
 
-                data_refer = data_set.get('datasetReference', {})
-                data_set_id = data_refer.get('datasetId', '')
-                dataset_project_id = data_refer.get('projectId')
+                data_refer = data_set.get("datasetReference", {})
+                data_set_id = data_refer.get("datasetId", "")
+                dataset_project_id = data_refer.get("projectId")
                 bq_dataset = big_query_conn.get_dataset(data_set_id)
-                creation_time = bq_dataset.get('creationTime', '')
-                last_modified_time = bq_dataset.get('lastModifiedTime')
-                region = self._get_region(bq_dataset.get('location', ''))
-                exp_partition_ms = bq_dataset.get('defaultPartitionExpirationMs')
-                exp_table_ms = bq_dataset.get('defaultTableExpirationMs')
+                creation_time = bq_dataset.get("creationTime", "")
+                last_modified_time = bq_dataset.get("lastModifiedTime")
+                region = self._get_region(bq_dataset.get("location", ""))
+                exp_partition_ms = bq_dataset.get("defaultPartitionExpirationMs")
+                exp_table_ms = bq_dataset.get("defaultTableExpirationMs")
 
                 # skip if dataset id is invisible
                 # if self._get_visible_on_console(data_set_id):
                 #     bq_dt_tables = big_query_conn.list_tables(data_set_id)
                 #     update_bq_dt_tables, table_schemas = self._get_table_list_with_schema(big_query_conn, bq_dt_tables)
 
-                labels = self.convert_labels_format(bq_dataset.get('labels', {}))
-                google_cloud_monitoring_filters = [{'key': 'resource.labels.dataset_id', 'value': data_set_id}]
+                labels = self.convert_labels_format(bq_dataset.get("labels", {}))
+                google_cloud_monitoring_filters = [
+                    {"key": "resource.labels.dataset_id", "value": data_set_id}
+                ]
 
                 ##################################
                 # 2. Make Base Data
                 ##################################
 
-                bq_dataset.update({
-                    'name': data_set_id,
-                    'project': project_id,
-                    'tables': update_bq_dt_tables,
-                    'table_schemas': table_schemas,
-                    'region': region,
-                    'visible_on_console': self._get_visible_on_console(data_set_id),
-                    'matching_projects': self._get_matching_project(dataset_project_id, projects),
-                    'creationTime': self._convert_unix_timestamp(creation_time),
-                    'lastModifiedTime': self._convert_unix_timestamp(last_modified_time),
-                    'default_partition_expiration_ms_display': self._convert_milliseconds_to_minutes(exp_partition_ms),
-                    'default_table_expiration_ms_display': self._convert_milliseconds_to_minutes(exp_table_ms),
-                    'labels': labels,
-                    'google_cloud_monitoring': self.set_google_cloud_monitoring(project_id,
-                                                                                "bigquery.googleapis.com",
-                                                                                data_set_id,
-                                                                                google_cloud_monitoring_filters)
-                })
+                bq_dataset.update(
+                    {
+                        "name": data_set_id,
+                        "project": project_id,
+                        "tables": update_bq_dt_tables,
+                        "table_schemas": table_schemas,
+                        "region": region,
+                        "visible_on_console": self._get_visible_on_console(data_set_id),
+                        "matching_projects": self._get_matching_project(
+                            dataset_project_id, projects
+                        ),
+                        "creationTime": self._convert_unix_timestamp(creation_time),
+                        "lastModifiedTime": self._convert_unix_timestamp(
+                            last_modified_time
+                        ),
+                        "default_partition_expiration_ms_display": self._convert_milliseconds_to_minutes(
+                            exp_partition_ms
+                        ),
+                        "default_table_expiration_ms_display": self._convert_milliseconds_to_minutes(
+                            exp_table_ms
+                        ),
+                        "labels": labels,
+                        "google_cloud_monitoring": self.set_google_cloud_monitoring(
+                            project_id,
+                            "bigquery.googleapis.com",
+                            data_set_id,
+                            google_cloud_monitoring_filters,
+                        ),
+                    }
+                )
                 big_query_data = BigQueryWorkSpace(bq_dataset, strict=False)
 
                 ##################################
                 # 3. Make Return Resource
                 ##################################
-                big_query_work_space_resource = SQLWorkSpaceResource({
-                    'name': data_set_id,
-                    'account': project_id,
-                    'region_code': region,
-                    'tags': labels,
-                    'data': big_query_data,
-                    'reference': ReferenceModel(big_query_data.reference())
-                })
+                big_query_work_space_resource = SQLWorkSpaceResource(
+                    {
+                        "name": data_set_id,
+                        "account": project_id,
+                        "region_code": region,
+                        "tags": labels,
+                        "data": big_query_data,
+                        "reference": ReferenceModel(big_query_data.reference()),
+                    }
+                )
 
                 ##################################
                 # 4. Make Collected Region Code
@@ -117,45 +141,57 @@ class SQLWorkspaceManager(GoogleCloudManager):
                 # 5. Make Resource Response Object
                 # List of SQLWorkSpaceResponse Object
                 ##################################
-                collected_cloud_services.append(SQLWorkSpaceResponse({'resource': big_query_work_space_resource}))
+                collected_cloud_services.append(
+                    SQLWorkSpaceResponse({"resource": big_query_work_space_resource})
+                )
             except Exception as e:
-                _LOGGER.error(f'[collect_cloud_service] => {e}', exc_info=True)
-                error_response = self.generate_resource_error_response(e, 'BigQuery', 'SQLWorkspace', data_set_id)
+                _LOGGER.error(f"[collect_cloud_service] => {e}", exc_info=True)
+                error_response = self.generate_resource_error_response(
+                    e, "BigQuery", "SQLWorkspace", data_set_id
+                )
                 error_responses.append(error_response)
 
-        _LOGGER.debug(f'** Big Query Finished {time.time() - start_time} Seconds **')
+        _LOGGER.debug(f"** Big Query Finished {time.time() - start_time} Seconds **")
         return collected_cloud_services, error_responses
 
     def _get_region(self, location):
         matched_info = self.match_region_info(location)
-        return matched_info.get('region_code') if matched_info else 'global'
+        return matched_info.get("region_code") if matched_info else "global"
 
-    def _get_table_list_with_schema(self, big_conn: SQLWorkspaceConnector, bq_dt_tables):
+    def _get_table_list_with_schema(
+        self, big_conn: SQLWorkspaceConnector, bq_dt_tables
+    ):
         update_bq_dt_tables = []
         table_schemas = []
         for bq_dt_table in bq_dt_tables:
-            table_ref = bq_dt_table.get('tableReference')
-            table_single = big_conn.get_tables(table_ref.get('datasetId'), table_ref.get('tableId'))
+            table_ref = bq_dt_table.get("tableReference")
+            table_single = big_conn.get_tables(
+                table_ref.get("datasetId"), table_ref.get("tableId")
+            )
 
             if table_single is not None:
-                creation_time = table_single.get('creationTime')
-                expiration_time = table_single.get('expirationTime')
-                last_modified_time = table_single.get('lastModifiedTime')
+                creation_time = table_single.get("creationTime")
+                expiration_time = table_single.get("expirationTime")
+                last_modified_time = table_single.get("lastModifiedTime")
 
-                table_single.update({
-                    'creationTime': self._convert_unix_timestamp(creation_time),
-                    'expirationTime': self._convert_unix_timestamp(expiration_time),
-                    'lastModifiedTime': self._convert_unix_timestamp(last_modified_time)
-                })
+                table_single.update(
+                    {
+                        "creationTime": self._convert_unix_timestamp(creation_time),
+                        "expirationTime": self._convert_unix_timestamp(expiration_time),
+                        "lastModifiedTime": self._convert_unix_timestamp(
+                            last_modified_time
+                        ),
+                    }
+                )
 
-                _table_schemas = table_single.get('schema', {})
+                _table_schemas = table_single.get("schema", {})
                 if _table_schemas != {}:
-                    fields = _table_schemas.get('fields', [])
-                    table_single.update({'schema': fields})
+                    fields = _table_schemas.get("fields", [])
+                    table_single.update({"schema": fields})
                     update_bq_dt_tables.append(table_single)
 
                     for single_schema in fields:
-                        single_schema.update({'table_id': table_ref.get('tableId')})
+                        single_schema.update({"table_id": table_ref.get("tableId")})
                         table_schemas.append(single_schema)
 
         return update_bq_dt_tables, table_schemas
@@ -164,18 +200,18 @@ class SQLWorkspaceManager(GoogleCloudManager):
     def _get_matching_project(project_id, projects):
         _projects = []
         for project in projects:
-            if project_id == project.get('id'):
+            if project_id == project.get("id"):
                 _projects.append(ProjectModel(project, strict=False))
         return _projects
 
     @staticmethod
     def _get_visible_on_console(dataset_id):
-        return False if dataset_id.startswith('_') else True
+        return False if dataset_id.startswith("_") else True
 
     @staticmethod
     def _convert_milliseconds_to_minutes(milliseconds):
         if milliseconds:
-            minutes = (int(milliseconds)/1000)/60
+            minutes = (int(milliseconds) / 1000) / 60
             return minutes
         else:
             return None
@@ -186,6 +222,6 @@ class SQLWorkspaceManager(GoogleCloudManager):
             try:
                 return datetime.fromtimestamp(int(unix_timestamp) / 1000)
             except Exception as e:
-                _LOGGER.error(f'[_convert_unix_timestamp] {e}')
+                _LOGGER.error(f"[_convert_unix_timestamp] {e}")
 
         return None
