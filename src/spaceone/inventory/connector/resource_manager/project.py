36b15cebd7084606a4dd6492b823da64d00deff4
kang2453 <kang2453@gmail.com>
2025-04-16 18:13:20 +0900

    src/spaceone/inventory/service/collector_service.py
    Signed-off-by: kang2453 <kang2453@gmail.com>

--- src/spaceone/inventory/connector/resource_manager/project.py
+++ src/spaceone/inventory/connector/resource_manager/project.py
@@ -15,5 +15,10 @@ class ProjectConnector(GoogleCloudConnector):
         self.secret_data = kwargs.get("secret_data", {})
 
     def get_project_info(self):
-        name = f"projects/{self.secret_data['project_id']}"
-        return self.client.projects().get(name=name).execute()
+        try:
+            _LOGGER.info(self.secret_data['project_id'])
+            name = f"projects/{self.secret_data['project_id']}"
+            return self.client.projects().get(name=name).execute()
+        except Exception as e:
+            _LOGGER.error(f"TTT to get project info: {e}", exc_info=True)
+            raise
