3a71934a35475e159033f8cea154b330b0d88b73
kyeongwook-kim <kyeonguk@mz.co.kr>
2025-09-09 15:22:57 +0900

    refactor: remove duplicate files and unused project connector

--- src/spaceone/inventory/connector/kms/__init__.py
+++ src/spaceone/inventory/connector/kms/__init__.py
@@ -1,4 +1,3 @@
-from spaceone.inventory.connector.kms.keyring_v1 import KMSKeyRingV1Connector
 from spaceone.inventory.connector.kms.kms_v1 import KMSV1Connector
 
-__all__ = ["KMSKeyRingV1Connector", "KMSV1Connector"]
+__all__ = ["KMSV1Connector"]
